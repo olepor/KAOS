846dfcb37f13bac985540dae4708c7d9a510caaf
Ole Petter <olepor@matnat.uio.no>
2019-07-09 17:46:21 +0200

    Created a precedenceMap for the PrattParser
    Signed-off-by: Ole Petter <olepor@matnat.uio.no>

--- src/prattparser.rs
+++ src/prattparser.rs
@@ -3,10 +3,29 @@
 use ast::*;
 
 enum Precedence {
-        LOWEST,
-        EQUALS, // == LESSGREATER // > or <
-        SUM, // +
-        PRODUCT, // *
-        PREFIX, // -X or !X
-        CALL, // myFunction(X)
-    }
+    LOWEST,
+    EQUALS,  // == LESSGREATER // > or <
+    LESSGREATER // < or > TODO - is the above wrong?
+    SUM,     // +
+    PRODUCT, // *
+    PREFIX,  // -X or !X
+    CALL,    // myFunction(X)
+}
+
+// PrecedenceMap table relates tokens to their precedence
+// through a hashmap (lazily initialized)
+lazy_static! {
+    static ref PrecedenceMap: HashMap<Token, Precedence> = {
+        let mut m : HashMap<&str, Token> = HashMap::new();
+        m.insert(Token::EQ, Precedence::EQUALS);
+        m.insert(Token::NOTEQ, Precedence::EQUALS);
+        m.insert(Token::LT, Precedence::LESSGREATER);
+        m.insert(Token::GT, Precedence::LESSGREATER);
+        m.insert(Token::PLUS, Precedence::SUM);
+        m.insert(Token::MINUS, Precedence::SUM);
+        m.insert(Token::SLASH, Precedence::PRODUCT);
+        m.insert(Token::ASTERISK, Precedence::PRODUCT);
+        m
+    };
+}
+
